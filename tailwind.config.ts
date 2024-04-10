3e47a147f7aee02233461e81a82266c86e9dd6d6
Lucas Angeli <jl.angeli@outlook.com>
2024-04-10 17:13:35 -0300

    tailwind css setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,76 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
+      fontSize: {
+        h1: '52px',
+        h2: '40px',
+        h3: '32px',
+        h4: '26px',
+        h5: '22px',
+        large: '20px',
+        regular: '16px',
+        'regular-sm': '14px',
+        subtitle: '12px',
+        'subtitle-sm': '10px',
+      },
+      colors: {
+        'black-50': '#646D85',
+        'black-60': '#3D4353',
+        'black-70': '#333844',
+        'black-80': '#2A2E39',
+        'black-90': '#252932',
+        'black-100': '#222429',
+        'secondary-60': '#E7E7E7',
+        'secondary-70': '#C1C5CF',
+        'secondary-80': '#7A7F8D',
+        'secondary-90': '#595B63',
+        'secondary-100': '#393D49',
+        'light-green': '#ADFF2F',
+        'medium-green': '#ADDF45',
+        'dark-green': '#93BC1E',
+        error: '#F96C6C',
+        white: '#FFFFFF',
+        'dashboard-bg': '#1E1E1E',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
 
-export default config
+export default config;
